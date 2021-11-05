a61d1a8476c79867a05d9d2bb1d482e223967da2
Maya_Khaled <may2yk@yahoo.com>
2021-11-05 22:08:32 +0200

    New structure

---
+++ Backend/src/index.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const connectDB = require("./database");
+const cors = require("cors");
+const admin = require("./Controllers/AdminController");
+const validateAddFlight = require("./Validation/FlightValidation");
+const port = 8000;
+const express = require("express");
+const app = express();
+//const jwt = require("jsonwebtoken");
+
+const { appendFile } = require("fs");
+app.use(express.json());
+
+app.post("/addFlight", validateAddFlight.validateAddFlight, admin.createFlight);
+
+app.listen(port);
+
+connectDB();
