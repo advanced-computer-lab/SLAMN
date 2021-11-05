f1a3db78dd10343c162520073e36508b51cc4c27
nourelgazzar <nourelgazzar2@gmail.com>
2021-11-05 22:51:46 +0200

    final structure

---
+++ Frontend/airlinesystem/src/Components/General/TextBox.js
@@ -0,0 +1,16 @@
+import React from "react";
+import TextField from "@material-ui/core/TextField";
+
+export default function TextBox(props) {
+  const [value, setValue] = React.useState("");
+
+  const onChange = (e) => {
+    setValue(e.target.value);
+  };
+
+  return (
+    <div>
+      <TextField variant="outlined" label={props.title} onChange={onChange} />
+    </div>
+  );
+}
