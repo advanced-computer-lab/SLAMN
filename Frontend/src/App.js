c5238790b859556667b0f6395072b731c532e2e8
nourelgazzar <nourelgazzar2@gmail.com>
2021-11-06 19:19:11 +0200

    last commit

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -3,13 +3,17 @@ import React from "react";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Update from "./Page/Update";
 import TextBox from "./Componenets/General/TextBox";
+import AdminHome from "../src/Page/AdminHome";
+import CreateFlight from "./Page/CreateFlight";
 
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Update />} />
+          <Route path="/update" element={<Update />} />
+          <Route path="/homeadmin" element={<AdminHome />} />
+          <Route path="/addflight" element={<CreateFlight />} />
         </Routes>
       </BrowserRouter>
     </div>
