996a9b394d4a231607948999d7e857e1359d9681
nourelgazzar <nourelgazzar2@gmail.com>
2021-11-06 17:01:58 +0200

    finaalll

--- Backend/src/Controller/FlightController.js
+++ Backend/src/Controller/FlightController.js
@@ -1,8 +1,28 @@
-const router = require('express').Router();
-const FlightServices = require("../Services/FlightServices")
+const router = require("express").Router();
+const FlightServices = require("../Services/FlightServices");
 const FlightValidation = require("../Middleware/FlightValidation");
 
-router.route('/').get(FlightServices.getFlights).post(FlightValidation.validateAddFlight, FlightServices.createFlight);
-router.route('/:id').put(FlightServices.updateFlight).delete(FlightServices.deleteFlight)
+router
+  .route("/")
+  .get(FlightServices.getFlights)
+  .post(FlightValidation.validateAddFlight, FlightServices.createFlight);
+router
+  .route("/:id")
+  .put(FlightServices.updateFlight)
+  .delete(FlightServices.deleteFlight);
+router
+  .route("/deleteFlight")
+  .post(FlightValidation.validateDeleteFlight, FlightServices.deleteFlight);
 
-module.exports=router;
+app.post(
+  "/searchFlight",
+  validateFlight.validateSearchFlight,
+  admin.searchFlight
+);
+app.post(
+  "/showavailableFlight",
+  validateFlight.validateShowAvailabeFlight,
+  admin.showAvailableFlight
+);
+
+module.exports = router;
